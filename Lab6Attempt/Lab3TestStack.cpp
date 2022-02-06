b97c514dac2ac767a24a19f0bd58ff910cd80741
Jason Wells <jason.wells@oit.edu>
2022-02-05 21:56:31 -0800

    Lab6 doing good work. Test time.

--- Lab6Attempt/Lab3TestStack.cpp
+++ Lab6Attempt/Lab3TestStack.cpp
@@ -1,4 +1,4 @@
-#include "ListStack.h"
+#include "ListQueue.h"
 #include <iostream>
 
 using namespace std;
@@ -7,25 +7,10 @@ using namespace data_structures;
 
 int main()
 {
-	ListStack<int> queue;
+	ListQueue<int> queue;
 	int x, y;
-	queue.initializeQueue();
+	
 	x = 4;
 	y = 5;
-	queue.addQueue(x);
-	queue.addQueue(y);
-	x = queue.front();
-	queue.deleteQueue();
-	queue.addQueue(x + 5);
-	queue.addQueue(16);
-	queue.addQueue(x);
-	queue.addQueue(y - 3);
-	cout << "Queue Elements: ";
-	while (!queue.isEmptyQueue())
-	{
-		cout << queue.front() << " ";
-		queue.deleteQueue();
-	}
-	cout << endl;
 	return 0;
 }
